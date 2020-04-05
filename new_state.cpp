aababba841583c3d7ba57c69827c65b11c6aa158
Broever101 <mrassam258@gmail.com>
2020-04-05 18:51:26 +0500

    file reading

--- new_state.cpp
+++ new_state.cpp
@@ -6,19 +6,36 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <errno.h>
+#include <vector>
 
 void writeToPipe(int, std::string&);
+typedef std::vector<std::string> s_vector;
+
+void split(const std::string &str, const char token,
+           s_vector &tokens){
+    size_t start = 0, end = 0;
+    while (end != std::string::npos){
+        end = str.find(token, start);
+        tokens.push_back(str.substr(start, end - start));
+        start = end + sizeof(token);
+    }
+}
 
 int main(int argc, char* argv[]){
-    int new_ready = open("new2ready", O_WRONLY);
-    if (new_ready < 0) std::cout<<"Could not open new2ready in new.\n";
-    else{
-        //std::cout<<"Writing to new2ready in new state.\n";
-        std::string message = "SMOKE THIS PIPE READY!";
-        writeToPipe(new_ready, message);
-        close(new_ready);
-    }  
+    //int new_ready = open("new2ready", O_WRONLY);
+    //if (new_ready < 0) exit(1);
+    s_vector tokens;
+    std::string file_path = "processes/Sample_2_FCFS.txt";
+    int fd = open(file_path.c_str(), O_RDONLY);
+    if (fd < 0) perror("Error :");
+    char kek[99];
+    int bytes = read(fd, kek, sizeof(kek));
+    split(kek, '\n', tokens);
+    close(fd);
 }
+
+
 
 void writeToPipe(int pipe_name, std::string& message){
     write(pipe_name, message.c_str(), message.size()+1);
