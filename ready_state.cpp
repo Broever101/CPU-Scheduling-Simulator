ae70e2f4d2a4dcf0cb891d7595f5dc9ae2dc81d9
Broever101 <mrassam258@gmail.com>
2020-04-09 19:09:42 +0500

    just wanna get outta here mang

--- ready_state.cpp
+++ ready_state.cpp
@@ -15,24 +15,44 @@ typedef std::vector<std::shared_ptr<Process>> p_vector;
 
 void writeToPipe(int, std::string&);
 std::string readFromPipe(int);
+std::string createPacket(const std::shared_ptr<Process>&);
 void createProcs(std::string data, p_vector&);
 
 int main(int argc, char* argv[]){
     int new_ready = open("new2ready", O_RDONLY);
     if (new_ready < 0) std::cout<<"Could not open new2ready in ready.\n";
+    int ready_running = open("ready2running", O_WRONLY);
+    if (ready_running < 0) std::cout<<"Could not open ready2running in ready.\n";
     
     std::string scheduling_algo = readFromPipe(new_ready);
     p_vector procs;
+    std::string packet;
+    size_t time = 0;
     std::string data = readFromPipe(new_ready);
-    while (data != "closed"){
+    while (data != "closed" && procs.size() != 0){
         createProcs(data, procs);
-        std::cout<<"Process scheduled:\n";
+        std::cout<<"PROCESS ARRIVED:\n";
+        std::cout<<(*procs.rbegin())->proc_name<<std::endl;
+        while (time != procs.begin()->get()->arrival){
+            sleep(1);
+            ++time;
+        }
+        packet = createPacket(*(procs.begin()));
+        writeToPipe(ready_running, packet);
+        std::cout<<"PROCESS SCHEDULED: "<<(*procs.begin())->proc_name<<std::endl;
+        procs.erase(procs.begin());
         std::cout<<(*procs.rbegin())->proc_name<<std::endl;
         data = readFromPipe(new_ready);
     }
     std::cout<<"CLOSED.\n";
     close(new_ready);
     exit(0);
+}
+
+std::string createPacket(const std::shared_ptr<Process>& proc){
+    return std::string(proc->proc_name + "\n" +
+                std::to_string(proc->arrival) + "\n" +
+                std::to_string(proc->burst));
 }
 
 void printVector(const p_vector& procs){
