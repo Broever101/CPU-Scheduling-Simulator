28c2d78d10d9e8ee48e5b92e35323432b41481eb
Broever101 <mrassam258@gmail.com>
2020-04-13 16:31:42 +0500

    Updated Processes

--- Process.h
+++ Process.h
@@ -4,6 +4,13 @@ public:
     std::string proc_name;
     size_t arrival;
     size_t burst;
+    size_t remaining_burst;
+    size_t turnaround;
+    size_t waiting;
     short priority = 0;
-    inline Process(std::string proc, size_t arr, size_t burst) : proc_name(proc), arrival(arr), burst(burst) {}
+    inline Process(std::string proc, size_t arr, size_t burst, size_t rem_burst, size_t turn, size_t wait) : 
+    proc_name(proc), arrival(arr), burst(burst), remaining_burst(rem_burst), turnaround(turn), waiting(wait) {}
+    bool operator==(const Process& obj){
+        return this->proc_name == obj.proc_name;
+    }
 };
